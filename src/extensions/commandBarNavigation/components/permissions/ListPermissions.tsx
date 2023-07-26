92aa272303e9f85d0924267d5df41ce2d8de0638
mmsharepoint <markus.moeller.ed@outlook.de>
2023-07-26 15:30:37 +0200

    Confirm Dialog added to list

--- src/extensions/commandBarNavigation/components/permissions/ListPermissions.tsx
+++ src/extensions/commandBarNavigation/components/permissions/ListPermissions.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
-import { IconButton } from '@fluentui/react/lib/Button';
+import { PrimaryButton, DefaultButton, IconButton } from '@fluentui/react/lib/Button';
+import { Dialog, DialogType, DialogFooter } from '@fluentui/react/lib/Dialog';
 import { IIconProps } from '@fluentui/react';
 import { List } from '@fluentui/react/lib/List';
 import { SPService } from "../../../../services/SPService";
@@ -10,9 +11,17 @@ import { IListPermissionsProps } from "./IListPermissionsProps";
 export const ListPermissions: React.FC<IListPermissionsProps> = (props) => {
   const [items, setItems] = React.useState<IPermissionItem[]>([]);
   const spService = new SPService(props.serviceScope);
+  const [dialog, setDialog] = React.useState<JSX.Element>();
   const redoBtn: IIconProps = { iconName: 'Redo' };
   const cancelBtn: IIconProps = { iconName: 'Cancel' };
   
+  const dialogContentProps = {
+    type: DialogType.normal,
+    title: 'Confirm Permission Change',
+    closeButtonAriaLabel: 'Close',
+    subText: '',
+  };
+
   const evalListPermissions = () => {
     spService.evalSiteListsPermInheritance(props.currentSiteUrl)
       .then((respItems) => {
@@ -20,7 +29,8 @@ export const ListPermissions: React.FC<IListPermissionsProps> = (props) => {
       });
   };
 
-  const breakPermissionInheritance = React.useCallback((listID: string) => {    
+  const breakPermissionInheritance = (listID: string) => { 
+    hideDialog();   
     spService.breakInheritListPermissions(props.currentSiteUrl, listID).then((resp) => {
       if (resp) {
         evalListPermissions();
@@ -29,9 +39,28 @@ export const ListPermissions: React.FC<IListPermissionsProps> = (props) => {
         // error
       }
     }); 
+  };
+
+  const hideDialog = () => {
+    setDialog(<React.Fragment></React.Fragment>);
+  };
+
+  const confirmBreakPermissions = React.useCallback((listID: string) => {
+    dialogContentProps.subText = 'Do you really want to break inherited list permissions?'
+    setDialog(<Dialog
+              hidden={false}
+              onDismiss={hideDialog}
+              dialogContentProps={dialogContentProps}
+            >
+              <DialogFooter>
+                <PrimaryButton onClick={() => breakPermissionInheritance(listID)} text="OK" />
+                <DefaultButton onClick={hideDialog} text="Cancel" />
+              </DialogFooter>
+            </Dialog>);
   }, [items]);
 
-  const inheritPermissions = React.useCallback((listID: string) => {
+  const inheritPermissions = (listID: string) => {
+    hideDialog();
     spService.reInheritListPermissions(props.currentSiteUrl, listID).then((resp) => {
       if (resp) {
         evalListPermissions();
@@ -40,6 +69,20 @@ export const ListPermissions: React.FC<IListPermissionsProps> = (props) => {
         // error
       }
     });
+  };
+
+  const confirmInheritPermissions = React.useCallback((listID: string) => {
+    dialogContentProps.subText = 'Do you really want to re-inherit list permissions?'
+    setDialog(<Dialog
+              hidden={false}
+              onDismiss={hideDialog}
+              dialogContentProps={dialogContentProps}
+            >
+              <DialogFooter>
+                <PrimaryButton onClick={() => inheritPermissions(listID)} text="OK" />
+                <DefaultButton onClick={hideDialog} text="Cancel" />
+              </DialogFooter>
+            </Dialog>);
   }, [items]);
 
   const onRenderCell = (item: IPermissionItem, index: number | undefined): JSX.Element => {
@@ -50,8 +93,8 @@ export const ListPermissions: React.FC<IListPermissionsProps> = (props) => {
           <div className={styles.itemPermission}>
             <span className={styles.txtPermission}>{item.permission}</span>
             {item.permission === "Unique"?
-              <IconButton iconProps={ redoBtn } title='Re-inherit permissions' onClick={ () => inheritPermissions(item.key) } />:
-              <IconButton iconProps={ cancelBtn } title='Stop inherit permissions' onClick={ () => breakPermissionInheritance(item.key) } />}
+              <IconButton iconProps={ redoBtn } title='Re-inherit permissions' onClick={ () => confirmInheritPermissions(item.key) } />:
+              <IconButton iconProps={ cancelBtn } title='Stop inherit permissions' onClick={ () => confirmBreakPermissions(item.key) } />}
           </div>
         </div>
       </div>
@@ -66,6 +109,8 @@ export const ListPermissions: React.FC<IListPermissionsProps> = (props) => {
     <div className={styles.listPermissions}>
       <h4>Lists</h4>
       <List items={items} onRenderCell={onRenderCell} />
+
+      {dialog}
     </div>
   )
 }
